0365434ba45b0a602e45ee6fe79633fcf9e3a99a
Vitor Andrade <vitormdandrade@gmail.com>
2026-06-30 14:06:00 -0300

    Fix: remove i18n config (unsupported in App Router)

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['es'],
-    defaultLocale: 'es',
-  },
+  /* config options here */
 };
 
 export default nextConfig;
